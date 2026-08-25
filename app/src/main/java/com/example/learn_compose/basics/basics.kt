cf14b2bc2477b2e9e557b4eeb285d8989dc60efb
Vraj Desai <desaivraj18@gmail.com>
2026-08-25 23:31:17 +0530

    new module added textview

---
+++ app/src/main/java/com/example/learn_compose/basics/basics.kt
@@ -0,0 +1,21 @@
+package com.example.learn_compose.basics
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+@Preview(showBackground = true)
+@Composable
+fun text() {
+    Text(
+        text = "Mission android",
+        color = Color.Blue,
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = FontFamily.Serif
+)
+}
